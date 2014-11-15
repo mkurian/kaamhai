9a2caf464173d61fdda9f4d14567450839ff1f18
charitha <charithah418@BANL121b92744.local>
2014-11-15 18:39:06 +0530

    Phone App IOS and Android
    Phone App IOS and Android Using Onsen framework

--- KaamhaiApp/platforms/android/ant-build/HelloWorld-debug-unaligned.apk.d
+++ KaamhaiApp/platforms/android/ant-build/HelloWorld-debug-unaligned.apk.d
@@ -2,3 +2,17 @@
 /Users/charithah418/Documents/Experimental coding/kaamhai/KaamhaiApp/platforms/android/ant-build/HelloWorld.ap_ \
 /Users/charithah418/Documents/Experimental coding/kaamhai/KaamhaiApp/platforms/android/ant-build/classes.dex \
 /Users/charithah418/Documents/Experimental coding/kaamhai/KaamhaiApp/platforms/android/CordovaLib/ant-build/classes.jar \
+/Users/charithah418/Documents/Experimental coding/kaamhai/KaamhaiApp/platforms/android/libs/couchbase-lite-java-core-1.0.3.1.jar \
+/Users/charithah418/Documents/Experimental coding/kaamhai/KaamhaiApp/platforms/android/libs/stateless4j-2.4.0.jar \
+/Users/charithah418/Documents/Experimental coding/kaamhai/KaamhaiApp/platforms/android/libs/couchbase-lite-java-listener-1.0.3.1.jar \
+/Users/charithah418/Documents/Experimental coding/kaamhai/KaamhaiApp/platforms/android/libs/couchbase-lite-java-javascript-1.0.3.1.jar \
+/Users/charithah418/Documents/Experimental coding/kaamhai/KaamhaiApp/platforms/android/libs/cbl_collator_so-1.0.3.1.jar \
+/Users/charithah418/Documents/Experimental coding/kaamhai/KaamhaiApp/platforms/android/libs/webserver-2-3.jar \
+/Users/charithah418/Documents/Experimental coding/kaamhai/KaamhaiApp/platforms/android/libs/couchbase-lite-android-source-1.0.3.1.jar \
+/Users/charithah418/Documents/Experimental coding/kaamhai/KaamhaiApp/platforms/android/libs/jackson-mapper-asl-1.9.2.jar \
+/Users/charithah418/Documents/Experimental coding/kaamhai/KaamhaiApp/platforms/android/libs/couchbase-lite-android-1.0.3.1.jar \
+/Users/charithah418/Documents/Experimental coding/kaamhai/KaamhaiApp/platforms/android/libs/jackson-core-asl-1.9.2.jar \
+/Users/charithah418/Documents/Experimental coding/kaamhai/KaamhaiApp/platforms/android/libs/rhino-1.7R3.jar \
+/Users/charithah418/Documents/Experimental coding/kaamhai/KaamhaiApp/platforms/android/libs/servlet-2-3.jar \
+/Users/charithah418/Documents/Experimental coding/kaamhai/KaamhaiApp/platforms/android/libs/couchbase-lite-android-javadocs-enterprise-1.0.3.1.jar \
+/Users/charithah418/Documents/Experimental coding/kaamhai/KaamhaiApp/platforms/android/libs/commons-io-2.0.1.jar \
---
+++ KaamhaiApp/platforms/ios/www/js/app.js
@@ -0,0 +1,92 @@
+(function(){
+  'use strict';
+  var module = angular.module('app', ['onsen']);
+
+  module.controller('AppController', function($scope, $data) {
+    $scope.doSomething = function() {
+      setTimeout(function() {
+        alert('Marked as watched');
+      }, 100);
+    };
+
+    $scope.login = function(){
+   
+       openFB.init({appId: '518182581658668', tokenStore: window.localStorage});
+
+        openFB.login(
+                function(response) {
+                    if(response.status === 'connected') {
+
+                        openFB.api({path: '/me', 
+                          success: function(resp){alert(resp.id);
+                          },
+                          error: function(error){alert("error"+error);}});
+
+                       // $.get('https://graph.facebook.com/me?access_token=CAAHXSMi1ZACwBAD6x2HeQqvAMYy1zOJaqilTCSj74zTvE6N0dqTe0u8VvgDLZBoMfMs1HjZAN86osIjGUD4UhbZBkNdcmiXSrWPbHWI0hDKFZCkGtSFmcspoLFaz0GgG1endb5qPAHDG4tKRV8s9UHDZCtNQY6UJ3kc9YchqyqSjEMrVoOMQvkObsiB2ocwyUo5OxrLUTBzE0qyNZBU3FjFx7u9gLtEbO4ZD',function(resp){alert(resp);});
+  
+                    
+                    } else {
+                        console.log('Facebook login failed: ' + response.error);
+                    }
+                }, {scope: 'email,read_stream,publish_stream'});
+       menu.setMainPage('page1.html', {closeMenu: true})
+    }
+  });
+
+  module.controller('DetailController', function($scope, $data) {
+    $scope.item = $data.selectedItem;
+  });
+
+  module.controller('MasterController', function($scope, $data) {
+    $scope.items = $data.items;  
+    
+    $scope.showDetail = function(index) {
+      var selectedItem = $data.items[index];
+      $data.selectedItem = selectedItem;
+      $scope.ons.navigator.pushPage('detail.html', {title : selectedItem.title});
+    };
+  });
+
+  module.factory('$data', function() {
+      var data = {};
+      
+      data.items = [
+          { 
+              title: 'Arun P',
+              label: '4h',
+              desc: 'very good',
+              rating: '4',
+              category:'Maid'
+          },
+          { 
+              title: 'Jaya',
+              label: '6h',
+              desc: 'cool',
+              rating: '5',
+              category:'Driver'
+          },
+          { 
+              title: 'Komal',
+              label: '1day ago',
+              desc: '',
+              rating:'3',
+              category:'Helper'
+          },
+          { 
+              title: 'Vidya',
+              label: '1day ago',
+              desc: '',
+              rating: '2',
+              category:'Maid'
+
+          }
+      ]; 
+
+      for (var d in data.items){
+        data.items[d].cat= data.items[d].category.substring(0,2).toUpperCase();
+      }
+     
+      return data;
+  });
+})();
+
