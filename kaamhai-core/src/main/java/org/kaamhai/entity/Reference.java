dea61f210eb50d7449adefe83ec241a02ce811f3
Merrin Kurian <merrin.kurian@gmail.com>
2014-11-19 10:57:46 +0530

    Surprise element

--- kaamhai-core/src/main/java/org/kaamhai/entity/Reference.java
+++ kaamhai-core/src/main/java/org/kaamhai/entity/Reference.java
@@ -6,6 +6,18 @@ package org.kaamhai.entity;
  */
 public class Reference {
 
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	public String getReferrerId() {
+		return referrerId;
+	}
+	public void setReferrerId(String referrerId) {
+		this.referrerId = referrerId;
+	}
 	String note;
 	String referrerId;
 }
